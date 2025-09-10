665e784f202c20f97e9bf6df7b6b1f3fb82142a3
Jung2Soft <48377181+Jung2Soft@users.noreply.github.com>
2025-09-10 13:14:06 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,2 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('fitcal-v1').then(cache => cache.addAll(['./','./index.html','./style.css','./app.js','./plan.js','./manifest.webmanifest'])));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
-});
+self.addEventListener('install',e=>{e.waitUntil(caches.open('v1').then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./plan.js','./manifest.webmanifest'])))}) 
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
