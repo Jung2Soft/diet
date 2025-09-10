f9b0e04e5c02a655da26b132e3d59a5d3900bb56
Jung2Soft <48377181+Jung2Soft@users.noreply.github.com>
2025-09-10 14:36:05 +0900

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-
-// PWA service worker 등록
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').catch((err) => {
-      console.error('SW registration failed:', err);
-    });
-  });
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+if('serviceWorker'in navigator){window.addEventListener('load',()=>{navigator.serviceWorker.register('/service-worker.js').catch(e=>console.error('SW fail',e))})}
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>)
